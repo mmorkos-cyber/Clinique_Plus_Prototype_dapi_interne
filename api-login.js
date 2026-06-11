6c6f0111805e1afcfb8e29037e785c59da2af688
Malick FALL <emfall5015@gmail.com>
2026-06-11 15:50:18 +0200

    Separation des responsabilités-function-connexion-fonction-recherche-user

--- api-login.js
+++ api-login.js
@@ -1,92 +1,84 @@
 const express = require("express");
 
-const {connexion} = require('./init-bd');  //recuperation des fonctions 
+const { connexion, login, getUserById } = require('./userModel');  //recuperation des fonctions 
 
 let db = connexion();
 
 const app = express();
 
 // Pour permettre à Express de parser le JSON envoyé par Bruno
-app.use(express.json());  
+app.use(express.json());
 
-app.post("/login", (req,res) =>{
-   const mail = req.body.mail;
-   const password = req.body.password;
-   
-let reqSql = `select id,mail,role from users where mail=? and password=?`;
 
-db.get(reqSql, [mail, password], (err, rows) => {
-    if(err)
-    {
-        return res.status(500).json({"message" : "Erreur base de données"});
+app.post("/login", async (req, res) => {
+    const mail = req.body.mail;
+    const password = req.body.password;
+    const user = await login(mail, password);
+
+    if (!user) {
+        return res.status(500).json({ "message": "Erreur base de données" });
     }
-    
-    if(!rows) {  // Identifiants non trouvés
-        return res.status(200).json({"message": "Identifiants invalides"});
-    }
-    return res.status(200).json({"message" : "Connexion réussie",
-         "user" : {
-            "id":rows.id,
-            "mail" : rows.mail,
-            "role":rows.role } 
-        });
-    
-});
 
+    return res.status(200).json({
+        "message": "Connexion réussie",
+        "user": {
+            "id": user.id,
+            "mail": user.mail,
+            "role": user.role
+        }
+    });
 })
-// rechercher un user avec son ID 
-app.get('/users/:userId', (req, res) => {
-     const userId = req.params.userId;
-     let reqSql = `select id,mail,role from users where id=?`;
 
-db.get(reqSql, [userId], (err, rows) => {
-    if(err)
-    {
-        return res.status(500).json({"message" : "Erreur base de données"});
+
+// rechercher un user avec son ID 
+app.get('/users/:userId', async (req, res) => {
+    const userId = req.params.userId;
+
+    const user = await getUserById(userId);
+
+    if (!user) {
+        return res.status(500).json({ "message": "Erreur base de données" });
     }
-    
-    if(!rows) {  // Utilisateur non trouvé
-        return res.status(404).json({"message": "Utilisateur non trouvé"});
-    }
-    return res.status(200).json({"message" : "Utilisateur trouvé",
-         "user" : {
-            "id":rows.id,
-            "mail" : rows.mail,
-            "role":rows.role } 
-        });
-    
-});
+
+    return res.status(200).json({
+        "message": "Utilisateur trouvé",
+        "user": {
+            "id": user.id,
+            "mail": user.mail,
+            "role": user.role
+        }
+    });
+
 });
 
 // Ajouter un utilisateur 
 
-app.post("/add-user", (req,res) =>{
-   const mail = req.body.mail;
-   const password = req.body.password;
-   const role = req.body.role ;
-   db.run("INSERT OR IGNORE INTO users (mail, password, role) VALUES (?, ?, ?) ;", [mail, password, role], function (err) {
+app.post("/add-user", (req, res) => {
+    const mail = req.body.mail;
+    const password = req.body.password;
+    const role = req.body.role;
+    db.run("INSERT OR IGNORE INTO users (mail, password, role) VALUES (?, ?, ?) ;", [mail, password, role], function (err) {
         if (err) {
             return res.status(500).json({
                 message: "Erreur base de données"
             });
         }
-        if(this.lastID == 0)
-            return res.status(500).json({"message": "Utilisateur non ajouté"}); 
+        if (this.lastID == 0)
+            return res.status(500).json({ "message": "Utilisateur non ajouté" });
 
-    return res.status(200).json({"message" : "Utilisateur ajouté avec succés",
-         "user" : {
-            "id" : this.lastID,
-            "mail" :mail,
-            "password" : password,
-            "role" : role
-         } 
+        return res.status(200).json({
+            "message": "Utilisateur ajouté avec succés",
+            "user": {
+                "id": this.lastID,
+                "mail": mail,
+                "password": password,
+                "role": role
+            }
         });
-});
+    });
 })
 
 // Lister tous les utilisateurs 
-
-
 
 app.listen(3000, () => {
     console.log("Serveur démarré sur le port 3000");
